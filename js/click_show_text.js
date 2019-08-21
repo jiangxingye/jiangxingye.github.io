6d1c78d6d0d3ed68472f468956d36c3ad092dfd5
jiangxingye <jiangxingye@outlook.com>
2019-08-21 10:12:17 +0800

    Site updateed: 2019-08-21 10:12:12

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -8,7 +8,7 @@ jQuery(document).ready(function($) {
         var x = e.pageX,
         y = e.pageY;
         $i.css({
-            "z-index": 5,
+            "z-index": 15,
             "top": y - 20,
             "left": x,
             "position": "absolute",
